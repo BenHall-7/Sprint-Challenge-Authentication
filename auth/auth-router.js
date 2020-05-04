9bf32a522447f2d7eb33b04e73dff442e50b5dde
BenHall-7 <benjaminjahall@gmail.com>
2020-05-03 23:55:38 -0400

    Register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,41 @@
 const router = require('express').Router();
+const db = require("../database/dbConfig");
+const status = require("http-status-codes");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', checkAuthBody, (req, res) => {
+  db("users")
+    .insert(req.credentials)
+    .then(([ind]) => {
+      res.status(status.CREATED).json({
+        message: "logged in!",
+        index: ind,
+        token: jwt.sign(req.credentials.password, process.env.SECRET),
+      });
+    })
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', checkAuthBody, (req, res) => {
   // implement login
 });
+
+function checkAuthBody(req, res, next) {
+  if (typeof req.body === "object") {
+    if (typeof req.body.username === "string"
+      && typeof req.body.password === "string") {
+        req.credentials = {
+          username: req.body.username,
+          password: bcrypt.hashSync(req.body.password, 12),
+        };
+        next()
+    } else {
+      res.status(status.BAD_REQUEST).json({error: "Request body missing username or password strings"})
+    }
+  } else {
+    res.status(status.BAD_REQUEST).json({error: "Request missing body"})
+  }
+}
 
 module.exports = router;
