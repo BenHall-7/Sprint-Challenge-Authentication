99ee98941044aeb00c76db0b51bc3ddad9763a94
BenHall-7 <benjaminjahall@gmail.com>
2020-05-04 02:11:07 -0400

    Fix auth middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,16 +1,17 @@
 const db = require("../database/dbConfig");
 const status = require("http-status-codes");
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   try {
     const decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
-      db("users")
-        .where({username: decoded.username, password: decoded.password})
-        .then(([user]) => {
-          req.user = user;
-          next()
-        })
-        .catch(() => { res.status(status.UNAUTHORIZED).json({ you: 'shall not pass!' }); })
+    db("users")
+      .where({username: decoded.username, password: decoded.password})
+      .then(([user]) => {
+        req.user = user;
+        next()
+      })
+      .catch(() => { res.status(status.UNAUTHORIZED).json({ you: 'shall not pass!' }); })
   } catch {
     res.status(status.UNAUTHORIZED).json({ you: 'shall not pass!' });
   }
